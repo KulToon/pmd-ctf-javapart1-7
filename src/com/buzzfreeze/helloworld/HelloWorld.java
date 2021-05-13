c6414fb741bd72bccc1830d5e9809eb53688c8c1
Lashang166 <bankteeradhol@gmail.com>
2021-05-13 17:10:35 +0700

    F

---
+++ src/com/buzzfreeze/helloworld/HelloWorld.java
@@ -0,0 +1,29 @@
+package com.buzzfreeze.helloworld;
+
+public class HelloWorld {
+	public static void main(String [] args) {
+		//lab 9 - 1
+//		for(int count = 0; count <= 20; count++) {
+//			System.out.println(count);
+//			if(count == 11) {				System.out.println(count);
+//				continue;
+//			}
+//			System.out.println("a");
+//		
+//		}
+		
+		//lab 9 - 2
+		
+		for(int i = 0; i < 20; i++) {
+			if(i == 2) {
+				continue;
+			}
+			System.out.println(i);
+		}
+		
+		
+		
+		
+	}
+	
+}
